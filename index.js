b22288e2a8355a388a2b640302a5d8992f05b5be
Viktor <86052868+sla6ak@users.noreply.github.com>
2021-08-08 02:42:31 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// https://api.telegram.org/bot1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg/getUpdates
+// https://api.telegram.org/bot1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg/sendMessage?chat_id=-1001597039560&text=
+
+// для кнопки - при событии нажатия - запустим функцию - внутри которой выберем класс инпута - и запишем его насильно в страничку бота.
+document.querySelector('button').onclick = function(){
+    let message = document.querySelector('.message').value;
+    let space = ": "
+    let message1 = document.querySelector('.message1').value;
+    const token = "1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg"
+    let url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-1001597039560&text='
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url + message + space + message1, true);
+    xhttp.send();
+}
