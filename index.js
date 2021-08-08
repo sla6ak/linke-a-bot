c172290d95b9b5417957578ee3077c6a2d56bacf
Viktor <86052868+sla6ak@users.noreply.github.com>
2021-08-08 05:32:20 +0300

    alert

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 // https://api.telegram.org/bot1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg/sendMessage?chat_id=-1001597039560&text=
 
 // для кнопки - при событии нажатия - запустим функцию - внутри которой выберем класс инпута - и запишем его насильно в страничку бота.
+
 document.querySelector('button').onclick = function(){
-    let message = document.querySelector('.message').value;
-    let space = ": "
-    let message1 = document.querySelector('.message1').value;
-    const token = "1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg"
-    let url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-1001597039560&text='
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url + message + space + message1, true);
-    xhttp.send();
+    if (document.querySelector('.message').value == "" || document.querySelector('.message').value == false){
+        alert("Введите ваше имя!");}
+    else{
+        let message = document.querySelector('.message').value;
+        let space = ": "
+        let message1 = document.querySelector('.message1').value;
+        const token = "1942760019:AAGKqQwhI6gdJj3AhDT_Vptt4odB130n8vg"
+        let url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-1001597039560&text='
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", url + message + space + message1, true);
+        xhttp.send();
+        alert("Отправленно!");
+}
 }
